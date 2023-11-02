afc65770ed916d769ea7b61e71e1983a6370261c
Lakshay Kaura <lakshaykaura@gmail.com>
2023-11-02 20:29:28 +0530

    Style: Added some styling

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,9 +2,28 @@ $(document).ready(function () {
     $('#donateBtn').on('click', function () {
         let qrCode = $('#donate-qr');
         qrCode.toggle();
+
+        let otherOptionsDiv = $('div.hideOnDonate');
+        otherOptionsDiv.toggle();
+
+        let backButtonImage = $('.back-button-img');
+        backButtonImage.toggle();
+
+        $(this).toggleClass('active-button');
+        if ($(this).hasClass('back-button')) {
+            $(this).removeClass('back-button');
+            $('.button-text', this).text("Donate Now");
+            $('#interval').focus();
+        } else {
+            $(this).addClass('back-button');
+            $('.button-text', this).text("Back");
+        }
+
+        setTimeout(() => {
+            $(this).removeClass('active-button');
+        }, 100);
     });
 
-    // Load saved interval when popup opens
     chrome.storage.sync.get('checkInterval', function (data) {
         let interval = data.checkInterval || 60000;
         $('#interval').val(interval / 1000);
@@ -12,10 +31,19 @@ $(document).ready(function () {
 
     $('#save').on('click', function () {
         let interval = parseInt($('#interval').val(), 10) * 1000;
-        chrome.storage.sync.set({ 'checkInterval': interval }, function () {
+        chrome.storage.sync.set({'checkInterval': interval}, function () {
             let messageDiv = $('#message');
             messageDiv.show();
-            messageDiv[0].scrollIntoView({ behavior: "smooth" });
+            messageDiv[0].scrollIntoView({behavior: "smooth"});
+
+            $(this).addClass('active-button');
+
+            $('#interval').focus();
+
+            setTimeout(() => {
+                $(this).removeClass('active-button');
+            }, 100);
+
             setTimeout(() => {
                 window.close();
             }, 3000);
